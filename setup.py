8922ef4a6bee51ca02879ff057be243f0700cd57
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-03-15 11:40:00 +1100

    Added config file with default camera intrinsic parameters

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'camera_2d_lidar_calibration'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Install launch files
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        # Install config files
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
     ],
     install_requires=['setuptools', 'opencv-python' ,'os'],
     zip_safe=True,
